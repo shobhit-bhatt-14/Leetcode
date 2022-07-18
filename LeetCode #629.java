742438ff3977802dd166d1828f91c01a5385137f
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2022-07-18 17:38:33 +0530

    update

---
+++ LeetCode #629.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int kInversePairs(int n, int k) {
+        if (k < 0 || k > n * (n - 1) / 2)
+            return 0;
+
+        if (k == 0 || k == n * (n - 1) / 2)
+            return 1;
+
+        long[][] dp = new long[n + 1][k + 1];
+        dp[2][0] = 1;
+        dp[2][1] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i][0] = 1;
+
+            for (int j = 1; j <= Math.min(k, i * (i - 1) / 2); j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+
+                if (j >= i)
+                    dp[i][j] -= dp[i - 1][j - i];
+
+                dp[i][j] = (dp[i][j] + 1000000007) % 1000000007;
+            }
+        }
+
+        return (int) dp[n][k];
+    }
+}
