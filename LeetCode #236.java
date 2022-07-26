4a19f2583e2c086b1d6d0cc9f135a19d49c31357
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2022-07-26 12:29:51 +0530

    update

---
+++ LeetCode #236.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val == p.val || root.val == q.val)
+            return root;
+
+        if (root.left == null && root.right == null)
+            return null;
+
+        TreeNode left = null;
+        TreeNode right = null;
+
+        if (root.left != null)
+            left = lowestCommonAncestor(root.left, p, q);
+        if (root.right != null)
+            right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return (left == null) ? right : left;
+    }
+}
