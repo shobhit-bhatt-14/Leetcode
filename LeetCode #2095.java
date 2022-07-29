378dde97f4ff7aab2c83c36ce8d086808f5c241f
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2022-07-29 18:13:49 +0530

    update

---
+++ LeetCode #2095.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = new ListNode();
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+}
